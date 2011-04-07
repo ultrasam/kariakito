39560fa72bf56ef95c9821ef7d8a0bf372de2a0e
Sócrates Medina <socrates.medina@gmail.com>
2011-04-06 22:06:46 -0430

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kariakito::Application.config.secret_token = '8c1f363da7bea72284f630d265466a270bcc5f805f8382b40fd45706351169106d3f94cf26dcac9536ad7916901e845ed3b11a765d6b67f2fc70c663987ad78a'
