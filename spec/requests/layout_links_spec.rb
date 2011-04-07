9cdea9ad22430101c9bd85e723a2cb4053e972c1
Sócrates Medina <socrates.medina@gmail.com>
2011-04-06 23:43:32 -0430

    Creado el Home Esqueleto

---
+++ spec/requests/layout_links_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+describe "LayoutLinks" do
+  
+  it "should have a Home page at '/'" do
+    get '/'
+    response.should have_selector('title', :content => "Home")
+  end
+  
+  it "should have the right links on the layouts" do
+    visit root_path
+    click_link "Home"
+    response.should have_selector('title', :content => "Home")
+  end
+end
