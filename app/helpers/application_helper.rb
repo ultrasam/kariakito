35e7e52cae616a2a8b6a9a2e719790718e728d1e
Sócrates Medina <socrates.medina@gmail.com>
2011-04-17 19:50:44 -0430

    Working on improving the menu

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,4 +13,20 @@ module ApplicationHelper
   def logo
     image_tag("logo.png", :alt => "Kariakito", :class => "logo")
   end
+
+  def facebook_on
+    image_tag("feisbuc_cursor_on.png", :alt => "facebook", :class => "facebook_img" )
+  end
+
+  def facebook_off
+    image_tag("feisbuc_cursor_off.png", :alt => "facebook", :class => "facebook_img" )
+  end
+
+  def twitter_on
+    image_tag("tuiter_cursor_on.png", :alt => "twitter", :class => "twitter_img")
+  end
+
+  def twitter_off
+    image_tag("tuiter_cursor_off.png", :alt => "twitter", :class => "twitter_img")
+  end
 end
